c2b3151a97a7a084a210d0e878784b3547224843
cadub <cadubas1@gmail.com>
2023-09-13 14:28:37 -0300

    Adicionando Projeto

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:hands_on/widgets/buttonglobal.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Page", style: TextStyle(
+            fontSize: 40,
+            fontFamily: AutofillHints.name,
+            color: Colors.white
+          ),),
+          backgroundColor: const Color.fromRGBO(125, 96, 161, 1),
+        ),
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.only(top: 100),
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: const Text("Every Purchase Will be Made With Pleasure", style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35
+                  ),),
+                ),
+        
+                SizedBox(height: MediaQuery.of(context).size.width *0.05,),
+
+                const Text("Buy and Enjoy", style: TextStyle(
+                  fontSize: 20
+                ),),
+
+                SizedBox(height: MediaQuery.of(context).size.width *0.05,),
+
+                const ButtonGlobalTeste(
+                  textButton: "Start Shopping", 
+                  widthButton: 320,
+                  colorButton: Color.fromRGBO(125, 96, 161, 1),
+                  textColor: Colors.white,
+                  heightButton: 50,
+                ),
+                
+                SizedBox(height: MediaQuery.of(context).size.width *0.09,),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const ButtonGlobalTeste(textButton: "Learn More", widthButton: 150, colorButton: Colors.white, textColor: Colors.black, heightButton: 50),
+                    
+                    if(MediaQuery.of(context).size.width > 600) SizedBox(width: MediaQuery.of(context).size.width * 0.009,),
+
+                    const ButtonGlobalTeste(textButton: "Login", widthButton: 150, colorButton: Colors.white, textColor: Colors.black, heightButton: 50)
+                  ],
+                )
+              ]
+            ),
+          ),
+        ),
+    
+      ),
+    );
+  }
+}
---
+++ lib/widgets/buttonglobal.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ButtonGlobalTeste extends StatelessWidget {
+  final String textButton;
+  final double widthButton;
+  final Color colorButton;
+  final Color textColor;
+  final double heightButton;
+
+  const ButtonGlobalTeste(
+      {Key? key,
+      required this.textButton,
+      required this.widthButton,
+      required this.colorButton,
+      required this.textColor,
+      required this.heightButton
+      })
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widthButton,
+      height: heightButton,
+      decoration: BoxDecoration(
+        border: Border.all(width: 2, color: const Color.fromRGBO(125, 96, 161, 1)),
+        borderRadius: BorderRadius.circular(30),
+        color: colorButton,
+      ),
+      child: TextButton(
+          onPressed: comecarShopping,
+          child: Text(
+            textButton,
+            style: TextStyle(
+                color: textColor, fontWeight: FontWeight.bold, fontSize: 20),
+          )),
+    );
+  }
+
+  void comecarShopping() {
+    // ignore: avoid_print
+    print("Voce começou a comprar");
+  }
+}
